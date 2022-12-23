ca65dc7f65409467dfd65a2d5060a02214c0586e
Hannes Hultergård <hannes.hultergard@gmail.com>
2022-12-23 10:56:38 +0100

    Recreate project

--- android/app/src/main/kotlin/com/hultergard/ranker/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.hultergard.ranker
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/hultergard/ranker/ranker/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.hultergard.ranker.ranker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
